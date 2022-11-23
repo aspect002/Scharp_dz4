9a3fc34f2255b18fe531a91c6d2fd7946a580a4a
Stas <stas.krivosheev.0054@gmail.com>
2022-11-23 22:56:21 +0300

    first commit

---
+++ dz_25/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */
+
+//создаём переменную А и Б и вкладывает в нее значение введенное с консоли
+Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = GetDegree(a, b);
+Console.WriteLine($"Число {a} в натуральной степени {b} равняется: {result}");
+
+
+int GetDegree(int a, int b)
+{
+    int degree = a;
+    for(int i = 1; i < b; i++)
+    {
+         degree = degree * a;
+    }
+    return degree;
+}
---
+++ dz_29/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19, 6, 1, 33  -> [1, 2, 5, 7, 19,6, 1, 33] */
+
+Console.Write("Введите длинну массива: ");
+int lengthMass = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = FillArray(lengthMass);
+PrintArray(arr);
+
+
+
+
+
+
+int[] FillArray(int length)
+{
+    int[] result = new int[length]; //создаём массив с именем ресалт и присваиваем ему пустой массив с длинной length
+
+   //1 действие     2 условие        3 действие
+    for(int i = 0; i < result.Length; i++) //сначало выполняется действие 1
+    {   //после проверка условия, если оно верно, то проваливаемся в тело цикла
+        Console.Write($"введите {i} элемент массива: ");
+        result[i] = Convert.ToInt32(Console.ReadLine());
+    } //после выполнения кода, происходит 3 действие плюсуется счётчик, и после снова возвращаемся к первому действию где i уже равен +1
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($"ваш массив: ");
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+}
